d81dc1abab94e1e92c71fd0148a8bf4f61c4821c
Luis <enyeluigui1688@gmail.com>
2022-10-25 11:03:28 -0500

    Clase martes 25

---
+++ 5.estructuraDecision/index.js
@@ -0,0 +1,8 @@
+//la estructura de decision
+//seguira un camino distinto
+//segun la condicion
+vocal='e'
+switch(vocal){
+    case 'a':'e':case 'i':case 'o':case 'u':
+        console.log('la letra ', vocal,',es una vocal') la letra e 
+}
---
+++ 6.estructuraCiclo/ejercisios.js
@@ -0,0 +1,17 @@
+//exsiten 3
+//averiguar su concepto aplicacion y ejercisios
+//for
+for(let i=0;i<=10;1++){
+    console.log(i)
+}
+//while
+let eval =true
+let eval12=''
+while(eval==true){
+    if (eval12=='salir'){
+        console.log('adios') adios
+        eval=false
+    }
+    console.log('hola') hola
+}
+//do while
